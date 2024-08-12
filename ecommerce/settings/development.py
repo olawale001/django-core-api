2840707981267ae1a0e54247168b25aff5564f99
olawa001 <abdullateefyusuf80@gmail.com>
2024-08-12 14:00:56 +0100

    first api

---
+++ ecommerce/settings/development.py
@@ -0,0 +1,14 @@
+from .base import *
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+    }
+}
