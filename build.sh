2a821b86b7923cf68784eaa9c395a8e5267bfb11
olawa001 <abdullateefyusuf80@gmail.com>
2024-08-30 15:52:56 +0100

    statibase

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ source venv/bin/activate
 pip install -r requirements.txt
 Python3.9 manage.py makemigrations
 Python3.9 manage.py migrate
-Python3.9 manage.py collectstatic
+Python3.9 manage.py collectstatic --noinput
