d6dcbed4a49db32b74f5ac54d5eff22d10893863
olawa001 <abdullateefyusuf80@gmail.com>
2024-08-30 16:21:42 +0100

    python

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ source venv/bin/activate
 
 
 pip install -r requirements.txt
-Python3.9 manage.py makemigrations
-Python3.9 manage.py migrate
-Python3.9 manage.py collectstatic --noinput
+Python3 manage.py makemigrations
+Python3 manage.py migrate
+Python3 manage.py collectstatic --noinput
