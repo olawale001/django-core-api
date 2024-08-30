88698f8ddee3f2c3be8b432844113b3a458b08ad
olawa001 <abdullateefyusuf80@gmail.com>
2024-08-30 15:15:33 +0100

    pgdatashapy

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 pip install -r requirements.txt
-Python3.9 manage.py makemigrations
-Python3.9 manage.py migrate
-Python3.9 manage.py collectstatic
+Python3.10 manage.py makemigrations
+Python3.10 manage.py migrate
+Python3.10 manage.py collectstatic
